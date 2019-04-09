df5f965c91d9ceb13e754d280d5fff370faa3bca
Daniel Ors <do7811@gmail.com>
2019-04-09 11:55:28 -0400

    completed upt to what program

--- drills.js
+++ drills.js
@@ -1,27 +1,177 @@
 'use strict';
 
-const BinarySearchTree = require('./binary');
+// const BinarySearchTree = require('./binary');
 
 // Draw a BST
 // Given the data 3,1,4,6,9,2,5,7, if you were to insert this into an empty binary search tree, what would the tree look like? 
 // (Draw the tree, no coding needed here.)
+
+// answer: [https://www.dropbox.com/s/lflr844xl73cui0/Screenshot%202019-04-09%2011.11.44.png?dl=0]
+
+
 // Draw the BST with the keys - E A S Y Q U E S T I O N
 
+// answer [ https://www.dropbox.com/s/7cduzz37vecdnxx/Screenshot%202019-04-09%2011.19.07.png?dl=0]
 
 // Remove the root
 // Show how the above trees would look like if you deleted the root of each tree. 
 // (Draw the trees, no coding needed here.)
 
+// answer 1: https://www.dropbox.com/s/2l6syfcurankfbm/Screenshot%202019-04-09%2011.21.44.png?dl=0
+// answer 2: https://www.dropbox.com/s/qzb1m9pz3twyihq/Screenshot%202019-04-09%2011.24.14.png?dl=0
 
 // Create a BST class
 // Walk through the binary search tree code in the curriculum and understand it well. 
 // Then write a BinarySearchTree class with its core functions (insert(), remove(), find()) from scratch.
 
+
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key == null) {
+      this.key = key;
+      this.value = value;
+    }
+    else if (key < this.key) {
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else{
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key == key) {
+      return this.value;
+    }
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    else {
+      throw new Error('Key Error in find');
+    }
+  }
+
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else{
+      throw new Error('Key error in remove');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+
+}
+
 // Create a binary search tree called BST and insert 3,1,4,6,9,2,5,7 into your tree. 
 // Compare your result with the result from the 1st exercise.
-
 // Create a binary search tree called BST and insert E A S Y Q U E S T I O N into your tree. 
 // Compare your result with the result from the 1st exercise.
+function main() {
+  let BST = new BinarySearchTree();
+
+  // BST.insert('3',3);
+  // BST.insert('1',1);
+  // BST.insert('4',4);
+  // BST.insert('6',6);
+  // BST.insert('9',9);
+  // BST.insert('2',2);
+  // BST.insert('5',5);
+  // BST.insert('7',7);
+
+  BST.insert('E', 'E');
+  BST.insert('A', 'A');
+  BST.insert('S', 'S');
+  BST.insert('Y', 'Y');
+  BST.insert('Q', 'Q');
+  BST.insert('U', 'U');
+  BST.insert('E', 'E');
+  BST.insert('S', 'S');
+  BST.insert('T', 'T');
+  BST.insert('I', 'I');
+  BST.insert('O', 'O');
+  BST.insert('N', 'N');
+
+  console.log(BST);
+}
+
+main();
 
 
 // What does this program do?
